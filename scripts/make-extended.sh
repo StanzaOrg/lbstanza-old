c4d925e9587e9d8a0c6a421516a421632760b44c
Patrick S. Li <patrickli.2001@gmail.com>
2015-06-27 14:37:31 -0700

    Merged resolver into extended stanza. Resolver now works on everything except but LoStanza.

--- scripts/make-extended.sh
+++ scripts/make-extended.sh
@@ -1,2 +1,2 @@
-stanza -e compiler/stz-langs.stanza compiler/lang-renamer.stanza -o build/estanzac -flags OPTIMIZE
+stanza -e compiler/stz-langs.stanza compiler/lang-renamer.stanza compiler/lang-resolver.stanza -o build/estanzac -flags OPTIMIZE
 build/estanzac -path /Users/psli/Docs/Programming/stanza -platform os-x -install estanza
