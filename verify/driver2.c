04f08df7abc1e39e7b13b7da1c238746c75aa3a8
Patrick Li <patrickli.2001@gmail.com>
2015-09-07 14:46:11 -0700

    Starting point. Entry point called from C and returning value.

---
+++ verify/driver2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+long stanza_entry (char* stack_mem);
+int main () {
+  char* stack_mem = malloc(1024 * 1024);
+  printf("Stack_mem = %p\n", stack_mem);
+  long x = stanza_entry(stack_mem);
+  float f = *(float*)&x;
+  double d = *(double*)&x;
+  printf("Return Long: %ld\n", x);
+  printf("Return Float: %f\n", f);
+  printf("Return Double: %lf\n", d);
+  return 0;
+}
