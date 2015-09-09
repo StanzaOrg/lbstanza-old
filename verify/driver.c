5cad2f99b190c0988275d17970105816d3aad9d9
Patrick S. Li <patrickli.2001@gmail.com>
2015-09-09 08:57:54 -0700

    Tested byte division.

--- verify/driver.c
+++ verify/driver.c
@@ -6,6 +6,8 @@ int main () {
   long x = myentry();
   float f = *(float*)&x;
   double d = *(double*)&x;
+  printf("Return Byte: %d\n", (char)x);
+  printf("Return Int: %d\n", (int)x);
   printf("Return Long: %ld\n", x);
   printf("Return Float: %f\n", f);
   printf("Return Double: %lf\n", d);
