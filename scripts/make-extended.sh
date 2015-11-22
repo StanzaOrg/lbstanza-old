ba44067413b29b2fe0d1654e46b376d6890b1496
Patrick Li <patrickli.2001@gmail.com>
2015-11-21 16:15:07 -0800

    Polished renamer pass. Top level LoStanza and HiStanza functions are in the same namespace now. Fields are also properly checked now.

--- scripts/make-extended.sh
+++ scripts/make-extended.sh
@@ -1,4 +1,4 @@
-stanza -e compiler/stz-langs.stanza compiler/lang-renamer.stanza compiler/lang-resolver.stanza compiler/lang-utils.stanza -o build/estanzac #-flags OPTIMIZE
+stanza -e compiler/stz-langs.stanza compiler/lang-renamer.stanza compiler/lang-resolver.stanza compiler/lang-utils.stanza -o build/estanzac -flags OPTIMIZE
 build/estanzac -path /Users/psli/Docs/Programming/stanza -platform os-x -install estanza
 
 
