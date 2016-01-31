fe1a1f0a74962d13db7ce682bba1f091f69dff43
Patrick Li <patrickli.2001@gmail.com>
2016-01-30 16:58:32 -0800

    Basic standard output stream functionality works.

--- boot/runtime/driver.c
+++ boot/runtime/driver.c
@@ -1,9 +1,15 @@
 #include<stdlib.h>
+#include<stdio.h>
 
 long stanza_entry (char* stack_mem);
 long stanza_stack_size;
+
 int main () {
   char* stack_mem = malloc(stanza_stack_size);
   stanza_entry(stack_mem);
   return 0;
 }
+
+FILE* get_stdout () {return stdout;}
+FILE* get_stderr () {return stderr;}
+FILE* get_stdin () {return stdin;}
