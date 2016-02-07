5fa0bb01a70cf435f917d9888856b7872930094c
Patrick Li <patrickli.2001@gmail.com>
2016-02-07 13:28:29 -0800

    Unsupported form error in compiler.

--- scripts/test-main.sh
+++ scripts/test-main.sh
@@ -1,3 +1,3 @@
-./build/stanzac -i boot/core/core.stanza boot/core/collections.stanza -o test.s
+./build/stanzac -i boot/core/core.stanza boot/core/collections.stanza boot/core/reader.stanza -o test.s
 gcc test.s boot/runtime/driver.c -o prog
 ./prog
