ec92c79282830c74757f12917b56c6b7251e248b
Patrick Li <patrickli.2001@gmail.com>
2016-02-08 11:38:53 -0800

    Ported stz-langs.stanza to new syntax.

---
+++ scripts/test-compiler.sh
@@ -0,0 +1,9 @@
+./build/stanzac -i boot/core/core.stanza \
+                   boot/core/collections.stanza \
+                   boot/core/reader.stanza \
+                   boot/core/macro-utils.stanza \
+                   boot/compiler/stz-il-ir.stanza \
+                   boot/tests/tests.stanza \
+                -o test.s
+gcc test.s boot/runtime/driver.c -o prog
+./prog
