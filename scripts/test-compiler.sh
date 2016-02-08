ed560debbc1869e48c8a645989893f0e7c8d6a73
Patrick S. Li <patrickli.2001@gmail.com>
2016-02-08 14:05:34 -0800

    Cast error on conversion to KForm.

--- scripts/test-compiler.sh
+++ scripts/test-compiler.sh
@@ -2,7 +2,9 @@
                    boot/core/collections.stanza \
                    boot/core/reader.stanza \
                    boot/core/macro-utils.stanza \
+                   boot/compiler/lang-read.stanza \
                    boot/compiler/stz-il-ir.stanza \
+                   boot/compiler/stz-input.stanza \
                    boot/tests/tests.stanza \
                 -o test.s
 gcc test.s boot/runtime/driver.c -o prog
