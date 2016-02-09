973e922b588a8e0ab166c462b9112161c060a2dd
Patrick Li <patrickli.2001@gmail.com>
2016-02-08 23:09:24 -0800

    Algorithms ported.

--- scripts/test-compiler.sh
+++ scripts/test-compiler.sh
@@ -1,10 +1,19 @@
+#./build/stanzac -i boot/core/core.stanza \
+#                   boot/core/collections.stanza \
+#                   boot/core/reader.stanza \
+#                   boot/core/macro-utils.stanza \
+#                   boot/compiler/stz-algorithms.stanza \
+#                   boot/compiler/lang-read.stanza \                   
+#                   boot/compiler/stz-il-ir.stanza \
+#                   boot/compiler/stz-input.stanza \
+#                   boot/tests/tests.stanza \
+#                -o test.s
+
 ./build/stanzac -i boot/core/core.stanza \
                    boot/core/collections.stanza \
                    boot/core/reader.stanza \
                    boot/core/macro-utils.stanza \
-                   boot/compiler/lang-read.stanza \
-                   boot/compiler/stz-il-ir.stanza \
-                   boot/compiler/stz-input.stanza \
+                   boot/compiler/stz-algorithms.stanza \
                    boot/tests/tests.stanza \
                 -o test.s
 gcc test.s boot/runtime/driver.c -o prog
