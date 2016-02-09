54d4a721827bf0594bdaceb181398e24b3113e4e
Patrick Li <patrickli.2001@gmail.com>
2016-02-09 12:04:32 -0800

    Parser passes compile. Running into segmentation fault.

--- scripts/test-compiler.sh
+++ scripts/test-compiler.sh
@@ -15,6 +15,7 @@
                    boot/core/macro-utils.stanza \
                    boot/compiler/stz-algorithms.stanza \
                    boot/compiler/stz-parser.stanza \
+                   boot/compiler/stz-core-macros.stanza \
                    boot/tests/tests.stanza \
                 -o test.s
 gcc test.s boot/runtime/driver.c -o prog
