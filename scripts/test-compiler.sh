e35790775eebc0f5e0b90cee8f40ce3d6ef3dac2
Patrick Li <patrickli.2001@gmail.com>
2016-02-10 14:12:10 -0800

    Added better error messages for improper format strings.

--- scripts/test-compiler.sh
+++ scripts/test-compiler.sh
@@ -1,14 +1,3 @@
-#./build/stanzac -i boot/core/core.stanza \
-#                   boot/core/collections.stanza \
-#                   boot/core/reader.stanza \
-#                   boot/core/macro-utils.stanza \
-#                   boot/compiler/stz-algorithms.stanza \
-#                   boot/compiler/lang-read.stanza \                   
-#                   boot/compiler/stz-il-ir.stanza \
-#                   boot/compiler/stz-input.stanza \
-#                   boot/tests/tests.stanza \
-#                -o test.s
-
 ./build/stanzac -i boot/core/core.stanza \
                    boot/core/collections.stanza \
                    boot/core/reader.stanza \
@@ -16,7 +5,11 @@
                    boot/compiler/stz-algorithms.stanza \
                    boot/compiler/stz-parser.stanza \
                    boot/compiler/stz-core-macros.stanza \
+                   boot/compiler/lang-read.stanza \
+                   boot/compiler/stz-il-ir.stanza \
+                   boot/compiler/stz-input.stanza \
                    boot/tests/tests.stanza \
                 -o test.s
+
 gcc test.s boot/runtime/driver.c -o prog
-./prog
+#./prog
