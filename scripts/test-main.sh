2cf1f76a52803d587137c03284f3a57e3a2fbcd1
Patrick Li <patrickli.2001@gmail.com>
2016-02-11 01:06:31 -0700

    Fixed bugs in lostanza core functions. Ptrs are very dangerous.

--- scripts/test-main.sh
+++ scripts/test-main.sh
@@ -1,3 +1,6 @@
-./build/stanzac -i boot/core/core.stanza boot/core/collections.stanza boot/core/reader.stanza boot/core/macro-utils.stanza boot/tests/tests.stanza -o test.s
-gcc test.s boot/runtime/driver.c -o prog
-./prog
+./build/stanzac -i boot/core/core.stanza \
+                   boot/core/collections.stanza \
+                   boot/tests/tests2.stanza \
+                -o test2.s
+
+gcc test2.s boot/runtime/driver.c -o prog2
