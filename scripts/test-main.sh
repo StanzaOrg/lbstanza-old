6ad06111e8604fb92a22e812ea1b8eac3d9efdae
Patrick Li <patrickli.2001@gmail.com>
2016-02-24 18:41:32 -0800

    Added ported syntax extension packages. Fixed bug in stz-main.

--- scripts/test-main.sh
+++ scripts/test-main.sh
@@ -1,6 +1,9 @@
 ./build/stanzac -i boot/core/core.stanza \
                    boot/core/collections.stanza \
-                   boot/compiler/stz-arg-parser.stanza \
+                   boot/core/reader.stanza \
+                   boot/core/macro-utils.stanza \
+                   boot/compiler/stz-algorithms.stanza \
+                   boot/compiler/stz-parser.stanza \
                    boot/tests/tests2.stanza \
                 -o test2.s
 
