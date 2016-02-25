e685cd3da449342200edd26be33711bba1aab2b1
Patrick S. Li <patrickli.2001@gmail.com>
2016-02-24 16:16:53 -0800

    Moved compiler syntax packages to syntax folder.

--- scripts/make-extended.sh
+++ scripts/make-extended.sh
@@ -1,4 +1,9 @@
-stanza -e compiler/stz-langs.stanza compiler/lang-renamer.stanza compiler/lang-resolver.stanza compiler/lang-utils.stanza -o build/estanzac -flags OPTIMIZE
+stanza -e syntax/stz-langs.stanza \
+          syntax/lang-renamer.stanza \
+          syntax/lang-resolver.stanza \
+          syntax/lang-utils.stanza \
+       -o build/estanzac \
+       -flags OPTIMIZE
 build/estanzac -path /Users/psli/Docs/Programming/stanza -platform os-x -install estanza
 
 
