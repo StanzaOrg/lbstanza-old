ca6e34377207481a87a2a6b6a771a9ee0e8af9c7
Patrick Li <patrickli.2001@gmail.com>
2016-02-26 16:44:20 -0800

    Got lambda lifting working.

--- scripts/test-main.sh
+++ scripts/test-main.sh
@@ -5,6 +5,7 @@
                    boot/compiler/stz-algorithms.stanza \
                    boot/compiler/stz-parser.stanza \
                    boot/tests/tests2.stanza \
-                -o test2.s
+                -o test2.s \
+                -optimize
 
 gcc test2.s boot/runtime/driver.c -o prog2
