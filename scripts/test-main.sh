89390904e557a13dcd7d5416d789642c299978c7
Patrick S. Li <patrickli.2001@gmail.com>
2016-02-28 01:16:47 -0800

    Fixed bug in inlining of marker type objects. Added equality check for marker types.

--- scripts/test-main.sh
+++ scripts/test-main.sh
@@ -1,11 +1,8 @@
 ./build/stanzac -i boot/core/core.stanza \
                    boot/core/collections.stanza \
-                   boot/core/reader.stanza \
-                   boot/core/macro-utils.stanza \
-                   boot/compiler/stz-algorithms.stanza \
-                   boot/compiler/stz-parser.stanza \
                    boot/tests/tests2.stanza \
                 -o test2.s \
                 -optimize
+
 
 gcc test2.s boot/runtime/driver.c -o prog2
