a8f56ec1b6d7e9d002404354c133f0d07a68b9ce
Patrick Li <patrickli.2001@gmail.com>
2016-02-29 13:19:46 -0800

    Added incremental production of output, to prevent out of heap errors.

--- scripts/test-main.sh
+++ scripts/test-main.sh
@@ -1,8 +1,7 @@
 ./build/stanzac -i boot/core/core.stanza \
                    boot/core/collections.stanza \
                    boot/tests/tests2.stanza \
-                -o test2.s
-
-
+                -o test2.s \
+                -optimize
 
 gcc test2.s boot/runtime/driver.c -o prog2
