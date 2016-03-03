2da0a5c6efbeda609682a3dc9b54a2c77fcd946f
Patrick Li <patrickli.2001@gmail.com>
2016-03-03 10:58:38 -0800

    Implemented a few optimizations.

--- scripts/test-main.sh
+++ scripts/test-main.sh
@@ -2,6 +2,6 @@
                    boot/core/collections.stanza \
                    boot/tests/tests2.stanza \
                 -o test2.s \
-                -optimize
+                -optimize 
 
 gcc test2.s boot/runtime/driver.c -o prog2
