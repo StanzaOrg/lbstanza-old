a9adf02ee95b2853ae485851c4bd5c4cb4720cd4
Patrick Li <patrickli.2001@gmail.com>
2016-03-03 22:09:38 -0800

    Added timer functionality to boot2.

--- boot2/runtime/driver.c
+++ boot2/runtime/driver.c
@@ -1,13 +1,19 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<sys/time.h>
 
+//     Stanza Defined Entities
+//     =======================
 long stanza_entry (char* stack_mem);
 extern long stanza_stack_size;
 
-//Command line arguments
+//     Command line arguments
+//     ======================
 long input_argc;
 char** input_argv;
 
+//     Main Driver
+//     ===========
 int main (int argc, char* argv[]) {
   input_argc = argc;
   input_argv = argv;
@@ -16,7 +22,23 @@ int main (int argc, char* argv[]) {
   return 0;
 }
 
+//     Macro Readers
+//     =============
 FILE* get_stdout () {return stdout;}
 FILE* get_stderr () {return stderr;}
 FILE* get_stdin () {return stdin;}
 int get_eof () {return EOF;}
+
+//     Time of Day
+//     ===========
+long current_time_us (void) {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec * 1000 * 1000 + tv.tv_usec;
+}
+
+long current_time_ms (void) {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
