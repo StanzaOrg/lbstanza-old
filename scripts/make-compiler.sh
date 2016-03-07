6f226e30c98f302c809231bc30b48427fd04cd29
Patrick Li <patrickli.2001@gmail.com>
2016-03-06 16:37:39 -0800

    BOOT: Implemented unstable pointers and rest types.

--- scripts/make-compiler.sh
+++ scripts/make-compiler.sh
@@ -1,3 +1,39 @@
+#./stanza core/reader.stanza \
+#         core/macro-utils.stanza \
+#         compiler/stz-algorithms.stanza \
+#         compiler/stz-padder.stanza \
+#         compiler/stz-utils.stanza \
+#         compiler/stz-parser.stanza \
+#         compiler/stz-params.stanza \
+#         compiler/stz-core-macros.stanza \
+#         compiler/stz-ids.stanza \
+#         compiler/lang-read.stanza \
+#         compiler/lang-check.stanza \
+#         compiler/stz-primitives.stanza \
+#         compiler/stz-il-ir.stanza \
+#         compiler/stz-tl-ir.stanza \
+#         compiler/stz-kl-ir.stanza \
+#         compiler/stz-tgt-ir.stanza \
+#         compiler/stz-bb-ir.stanza \
+#         compiler/stz-asm-ir.stanza \
+#         compiler/stz-input.stanza \
+#         compiler/stz-namemap.stanza \
+#         compiler/stz-renamer.stanza \
+#         compiler/stz-resolver.stanza \
+#         compiler/stz-infer.stanza \
+#         compiler/stz-type-calculus.stanza \
+#         compiler/stz-type.stanza \
+#         compiler/stz-compiler.stanza \
+#         compiler/stz-arg-parser.stanza \
+#         compiler/stz-langs.stanza  \
+#         compiler/lang-renamer.stanza \
+#         compiler/lang-resolver.stanza \
+#         compiler/stz-main.stanza \
+#      -o bin/$1 \
+#      -optimize
+
+
+
 ./stanza core/reader.stanza \
          core/macro-utils.stanza \
          compiler/stz-algorithms.stanza \
