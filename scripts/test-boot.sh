2d743754735511eb31d2b472ad05b7b1778e9029
Patrick S. Li <patrickli.2001@gmail.com>
2016-03-07 13:43:05 -0800

    DBG: Printing messages for debugging compiler.

---
+++ scripts/test-boot.sh
@@ -0,0 +1,8 @@
+./bin/stanzab boot/core/core.stanza \
+                boot/core/collections.stanza \
+                boot/tests/tests2.stanza \
+             -s test.s \
+             -o test \
+             -no-implicits \
+             -verbose
+
