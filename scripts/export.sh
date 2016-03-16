c48d4d6bf99f0e8d3f99cf221514afec3ab60387
Patrick S. Li <patrickli.2001@gmail.com>
2016-03-16 00:02:00 -0700

    Added scripts for exporting

---
+++ scripts/export.sh
@@ -0,0 +1,9 @@
+hg archive $1
+mv -f $1/notes $1/scripts $1/tests ~/.Trash/
+mv lstanza $1/lstanza
+cd $1
+zip -r stanza.zip compiler core docs runtime stanza
+mv stanza stanzabak
+mv lstanza stanza
+zip -r lstanza.zip compiler core docs runtime stanza
+mv -f compiler core docs runtime stanza stanzabak ~/.Trash/
