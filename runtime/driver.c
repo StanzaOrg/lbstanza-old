0c5440aa47112275b22210173150708f12644178
Patrick S. Li <patrickli.2001@gmail.com>
2016-03-23 17:35:27 -0700

    Prepare for merge.

---
+++ runtime/driver.c
@@ -0,0 +1,46 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<sys/time.h>
+#include<errno.h>
+
+//     Stanza Defined Entities
+//     =======================
+long stanza_entry (char* stack_mem);
+extern long stanza_stack_size;
+
+//     Command line arguments
+//     ======================
+long input_argc;
+char** input_argv;
+
+//     Main Driver
+//     ===========
+int main (int argc, char* argv[]) {  
+  input_argc = argc;
+  input_argv = argv;
+  char* stack_mem = (char*)malloc(stanza_stack_size);
+  stanza_entry(stack_mem);
+  return 0;
+}
+
+//     Macro Readers
+//     =============
+FILE* get_stdout () {return stdout;}
+FILE* get_stderr () {return stderr;}
+FILE* get_stdin () {return stdin;}
+int get_eof () {return EOF;}
+int get_errno () {return errno;}
+
+//     Time of Day
+//     ===========
+long current_time_us (void) {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec * 1000 * 1000 + tv.tv_usec;
+}
+
+long current_time_ms (void) {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
---
+++ scripts/test-boot.sh
@@ -0,0 +1,7 @@
+./bin/stanzadev boot/core/core.stanza \
+                boot/core/collections.stanza \
+                boot/tests/tests2.stanza \
+             -o test \
+             -no-implicits \
+             -verbose
+
