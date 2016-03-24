8d4dc0f259354138d63a1c0687198981739d8c5e
Patrick Li <patrickli.2001@gmail.com>
2016-03-24 11:10:25 -0700

    Carried over packages to KForm, implemented package flattening. Test programs still compile.

--- scripts/make-compiler.sh
+++ scripts/make-compiler.sh
@@ -1,3 +1,38 @@
+#./stanza core/reader.stanza \
+#         core/macro-utils.stanza \
+#         compiler/stz-algorithms.stanza \
+#         compiler/stz-padder.stanza \
+#         compiler/stz-utils.stanza \
+#         compiler/stz-parser.stanza \
+#         compiler/stz-params.stanza \
+#         compiler/stz-core-macros.stanza \
+#         compiler/stz-ids.stanza \
+#         compiler/lang-read.stanza \
+#         compiler/lang-check.stanza \
+#         compiler/stz-primitives.stanza \
+#         compiler/stz-il-ir.stanza \
+#         compiler/stz-tl-ir.stanza \
+#         compiler/stz-pkg-ir.stanza \
+#         compiler/stz-kl-ir.stanza \
+#         compiler/stz-tgt-ir.stanza \
+#         compiler/stz-bb-ir.stanza \
+#         compiler/stz-asm-ir.stanza \
+#         compiler/stz-input.stanza \
+#         compiler/stz-namemap.stanza \
+#         compiler/stz-renamer.stanza \
+#         compiler/stz-resolver.stanza \
+#         compiler/stz-infer.stanza \
+#         compiler/stz-type-calculus.stanza \
+#         compiler/stz-type.stanza \
+#         compiler/stz-kform.stanza \
+#         compiler/stz-compiler.stanza \
+#         compiler/stz-arg-parser.stanza \
+#         compiler/stz-langs.stanza  \
+#         compiler/lang-renamer.stanza \
+#         compiler/lang-resolver.stanza \
+#         compiler/stz-main.stanza \
+#      -o bin/$1
+
 ./stanza core/reader.stanza \
          core/macro-utils.stanza \
          compiler/stz-algorithms.stanza \
@@ -17,6 +52,7 @@
          compiler/stz-tgt-ir.stanza \
          compiler/stz-bb-ir.stanza \
          compiler/stz-asm-ir.stanza \
+         compiler/stz-backend.stanza \
          compiler/stz-input.stanza \
          compiler/stz-namemap.stanza \
          compiler/stz-renamer.stanza \
@@ -24,6 +60,10 @@
          compiler/stz-infer.stanza \
          compiler/stz-type-calculus.stanza \
          compiler/stz-type.stanza \
+         compiler/stz-kform.stanza \
+         compiler/stz-tgt.stanza \
+         compiler/stz-bb.stanza \
+         compiler/stz-asm-emitter.stanza \
          compiler/stz-compiler.stanza \
          compiler/stz-arg-parser.stanza \
          compiler/stz-langs.stanza  \
@@ -32,42 +72,4 @@
          compiler/stz-main.stanza \
       -o bin/$1
 
-#./stanza core/reader.stanza \
-#         core/macro-utils.stanza \
-#         compiler/stz-algorithms.stanza \
-#         compiler/stz-padder.stanza \
-#         compiler/stz-utils.stanza \
-#         compiler/stz-parser.stanza \
-#         compiler/stz-params.stanza \
-#         compiler/stz-core-macros.stanza \
-#         compiler/stz-ids.stanza \
-#         compiler/lang-read.stanza \
-#         compiler/lang-check.stanza \
-#         compiler/stz-primitives.stanza \
-#         compiler/stz-il-ir.stanza \
-#         compiler/stz-tl-ir.stanza \
-#         compiler/stz-kl-ir.stanza \
-#         compiler/stz-tgt-ir.stanza \
-#         compiler/stz-bb-ir.stanza \
-#         compiler/stz-asm-ir.stanza \
-#         compiler/stz-backend.stanza \
-#         compiler/stz-input.stanza \
-#         compiler/stz-namemap.stanza \
-#         compiler/stz-renamer.stanza \
-#         compiler/stz-resolver.stanza \
-#         compiler/stz-infer.stanza \
-#         compiler/stz-type-calculus.stanza \
-#         compiler/stz-type.stanza \
-#         compiler/stz-kform.stanza \
-#         compiler/stz-tgt.stanza \
-#         compiler/stz-bb.stanza \
-#         compiler/stz-asm-emitter.stanza \
-#         compiler/stz-compiler.stanza \
-#         compiler/stz-arg-parser.stanza \
-#         compiler/stz-langs.stanza  \
-#         compiler/lang-renamer.stanza \
-#         compiler/lang-resolver.stanza \
-#         compiler/stz-main.stanza \
-#      -o bin/$1 \
-#      -optimize
-#
+
