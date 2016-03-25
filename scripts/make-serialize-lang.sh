50680ced201144e5a0ade4ce44e9704b32a0727a
Patrick S. Li <patrickli.2001@gmail.com>
2016-03-24 17:34:11 -0700

    Started defining serializer DSL for pkg files.

---
+++ scripts/make-serialize-lang.sh
@@ -0,0 +1,8 @@
+./stanza core/reader.stanza \
+         core/macro-utils.stanza \
+         compiler/stz-algorithms.stanza \
+         compiler/stz-parser.stanza \
+         compiler/stz-params.stanza \
+         compiler/stz-core-macros.stanza \
+         compiler/lang-serializer.stanza \
+      -o serialize-lang
