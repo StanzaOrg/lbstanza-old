5d4877d8dd5f5446e2770850d6980cb652fcf4c4
Patrick S. Li <patrickli.2001@gmail.com>
2016-04-05 20:10:44 -0700

    Dev: Standalone program for register allocation.

---
+++ scripts/export.sh
@@ -0,0 +1,11 @@
+if [ $# -lt 1 ]; then
+    echo "Not enough arguments"
+    exit 2
+fi
+hg archive $1
+cp lstanza $1/lstanza
+cd $1
+zip -r stanza.zip compiler core docs runtime stanza
+mv lstanza stanza
+zip -r lstanza.zip compiler core docs runtime stanza
+
---
+++ scripts/make-serialize-lang.sh
@@ -0,0 +1,10 @@
+./stanza core/reader.stanza \
+         core/macro-utils.stanza \
+         compiler/stz-algorithms.stanza \
+         compiler/stz-parser.stanza \
+         compiler/stz-params.stanza \
+         compiler/stz-core-macros.stanza \
+         compiler/lang-serializer.stanza \
+      -o serialize-lang \
+      -flags TESTING
+
