f4c51084a834192b92f39924af12c1c335911fff
Patrick Li <patrickli.2001@gmail.com>
2016-04-11 17:43:11 -0700

    Updated export script to include License file. Compiled bootstrap compiler.

--- scripts/export.sh
+++ scripts/export.sh
@@ -5,7 +5,8 @@ fi
 hg archive $1
 cp lstanza $1/lstanza
 cd $1
-zip -r stanza.zip compiler core docs runtime stanza
+zip -r stanza.zip compiler core docs runtime License.txt stanza
 mv lstanza stanza
-zip -r lstanza.zip compiler core docs runtime stanza
+zip -r lstanza.zip compiler core docs runtime License.txt stanza
+mv stanza.zip lstanza.zip ~/Desktop/
 
