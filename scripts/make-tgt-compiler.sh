58587695e7e0a8675054b193bf09c2c2bf631c49
Patrick S. Li <patrickli.2001@gmail.com>
2016-04-15 11:03:40 -0700

    Got separate compilation branch working again.

---
+++ scripts/make-tgt-compiler.sh
@@ -0,0 +1,18 @@
+./stanza \
+     compiler/stz-params.stanza \
+     compiler/stz-utils.stanza \
+     compiler/stz-ids.stanza \
+     compiler/stz-algorithms.stanza \
+     compiler/stz-padder.stanza \
+     compiler/stz-backend.stanza \
+     compiler/stz-imms.stanza \
+     compiler/stz-tgt-ir.stanza \
+     compiler/stz-bb-ir.stanza \
+     compiler/stz-asm-ir.stanza \
+     compiler/stz-tgt-writer.stanza \
+     compiler/stz-bb.stanza \
+     compiler/stz-tgt-compiler.stanza \
+  -o tgt-compiler \
+
+
+
