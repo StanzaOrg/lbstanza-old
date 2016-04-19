2dc6f696d4800e75b35d7b5d140e9cf0f4b4011f
Patrick Li <patrickli.2001@gmail.com>
2016-04-19 11:34:28 -0700

    Updated version number. Wrote make script.

--- scripts/export.sh
+++ scripts/export.sh
@@ -5,8 +5,8 @@ fi
 hg archive $1
 cp lstanza $1/lstanza
 cd $1
-zip -r stanza.zip compiler core docs runtime License.txt stanza
+zip -r stanza.zip pkgs fast-pkgs compiler core docs runtime License.txt stanza
 mv lstanza stanza
-zip -r lstanza.zip compiler core docs runtime License.txt stanza
+zip -r lstanza.zip pkgs fast-pkgs compiler core docs runtime License.txt stanza
 mv stanza.zip lstanza.zip ~/Desktop/
 rm -rf *
