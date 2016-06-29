4c49ab891bfc2e8c3459c9076ccddb92c0d67b7d
Patrick S. Li <patrickli.2001@gmail.com>
2016-06-29 14:20:59 -0700

    Attempt to compile with new parser package. Compiler Crashing.

--- scripts/make-compiler.sh
+++ scripts/make-compiler.sh
@@ -1,3 +1,8 @@
+# USAGES:
+# ./make-compiler
+# ./make-compiler bin/stanzadev
+# ./make-compiler ./stanzac bin/stanzadev
+
 if [ $# -eq 0 ]
 then
     STANZA=./stanza
@@ -16,11 +21,11 @@ mkdir -p bin
 $STANZA  core/core.stanza \
          core/collections.stanza \
          core/reader.stanza \
+         core/parser.stanza \
          core/macro-utils.stanza \
          compiler/stz-algorithms.stanza \
          compiler/stz-padder.stanza \
          compiler/stz-utils.stanza \
-         compiler/stz-parser.stanza \
          compiler/stz-serializer.stanza \
          compiler/stz-params.stanza \
          compiler/stz-core-macros.stanza \
@@ -57,4 +62,51 @@ $STANZA  core/core.stanza \
          compiler/lang-resolver.stanza \
          compiler/lang-serializer.stanza \
          compiler/stz-main.stanza \
+      -pkg-path no-pkgs \
       -o $OUT
+
+#$STANZA  core/core.stanza \
+#         core/collections.stanza \
+#         core/reader.stanza \
+#         core/macro-utils.stanza \
+#         compiler/stz-algorithms.stanza \
+#         compiler/stz-padder.stanza \
+#         compiler/stz-utils.stanza \
+#         compiler/stz-parser.stanza \
+#         compiler/stz-serializer.stanza \
+#         compiler/stz-params.stanza \
+#         compiler/stz-core-macros.stanza \
+#         compiler/stz-ids.stanza \
+#         compiler/lang-read.stanza \
+#         compiler/lang-check.stanza \
+#         compiler/stz-primitives.stanza \
+#         compiler/stz-imms.stanza \
+#         compiler/stz-il-ir.stanza \
+#         compiler/stz-tl-ir.stanza \
+#         compiler/stz-pkg-ir.stanza \
+#         compiler/stz-kl-ir.stanza \
+#         compiler/stz-tgt-ir.stanza \
+#         compiler/stz-bb-ir.stanza \
+#         compiler/stz-asm-ir.stanza \
+#         compiler/stz-backend.stanza \
+#         compiler/stz-pkg.stanza \
+#         compiler/stz-input.stanza \
+#         compiler/stz-namemap.stanza \
+#         compiler/stz-renamer.stanza \
+#         compiler/stz-resolver.stanza \
+#         compiler/stz-infer.stanza \
+#         compiler/stz-type-calculus.stanza \
+#         compiler/stz-type.stanza \
+#         compiler/stz-kform.stanza \
+#         compiler/stz-tgt.stanza \
+#         compiler/stz-tgt-writer.stanza \
+#         compiler/stz-bb.stanza \
+#         compiler/stz-asm-emitter.stanza \
+#         compiler/stz-compiler.stanza \
+#         compiler/stz-arg-parser.stanza \
+#         compiler/stz-langs.stanza  \
+#         compiler/lang-renamer.stanza \
+#         compiler/lang-resolver.stanza \
+#         compiler/lang-serializer.stanza \
+#         compiler/stz-main.stanza \
+#      -o $OUT
