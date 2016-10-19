8506592b5a175f490e18f3862b6e4ac62f4ca783
Patrick S. Li <patrickli.2001@gmail.com>
2016-10-19 10:56:06 -0700

    Isolated frontend. Compiles.

--- scripts/make-compiler.sh
+++ scripts/make-compiler.sh
@@ -18,6 +18,7 @@ then
 fi
 
 mkdir -p bin
+
 $STANZA  core/core.stanza \
          core/collections.stanza \
          core/reader.stanza \
@@ -36,13 +37,7 @@ $STANZA  core/core.stanza \
          compiler/stz-imms.stanza \
          compiler/stz-il-ir.stanza \
          compiler/stz-tl-ir.stanza \
-         compiler/stz-pkg-ir.stanza \
          compiler/stz-kl-ir.stanza \
-         compiler/stz-tgt-ir.stanza \
-         compiler/stz-bb-ir.stanza \
-         compiler/stz-asm-ir.stanza \
-         compiler/stz-backend.stanza \
-         compiler/stz-pkg.stanza \
          compiler/stz-input.stanza \
          compiler/stz-namemap.stanza \
          compiler/stz-renamer.stanza \
@@ -50,11 +45,7 @@ $STANZA  core/core.stanza \
          compiler/stz-infer.stanza \
          compiler/stz-type-calculus.stanza \
          compiler/stz-type.stanza \
-         compiler/stz-kform.stanza \
-         compiler/stz-tgt.stanza \
-         compiler/stz-tgt-writer.stanza \
-         compiler/stz-bb.stanza \
-         compiler/stz-asm-emitter.stanza \
+         compiler/stz-type-to-kform.stanza \
          compiler/stz-compiler.stanza \
          compiler/stz-arg-parser.stanza \
          compiler/stz-langs.stanza  \
@@ -62,17 +53,16 @@ $STANZA  core/core.stanza \
          compiler/lang-resolver.stanza \
          compiler/lang-serializer.stanza \
          compiler/stz-main.stanza \
-      -pkg-path no-pkgs \
       -o $OUT
 
 #$STANZA  core/core.stanza \
 #         core/collections.stanza \
 #         core/reader.stanza \
+#         core/parser.stanza \
 #         core/macro-utils.stanza \
 #         compiler/stz-algorithms.stanza \
 #         compiler/stz-padder.stanza \
 #         compiler/stz-utils.stanza \
-#         compiler/stz-parser.stanza \
 #         compiler/stz-serializer.stanza \
 #         compiler/stz-params.stanza \
 #         compiler/stz-core-macros.stanza \
