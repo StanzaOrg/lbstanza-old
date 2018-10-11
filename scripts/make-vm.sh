9aff504ef37a067869bb8b9e4312a451a90465ce
Patrick S. Li <patrickli.2001@gmail.com>
2018-10-11 09:57:51 -0700

    Added built-ins file.

--- scripts/make-vm.sh
+++ scripts/make-vm.sh
@@ -1,9 +1,9 @@
 if [ $1 == "vm" ]
 then    
-  stanza compiler/stz-vm-ir.stanza compiler/stz-vm.stanza tests/bindings.stanza compiler/stz-vm-driver.stanza -o vm -ccfiles tests/mycfunctions.c
+  stanza compiler/stz-vm-ir.stanza compiler/stz-vm.stanza compiler/stz-built-ins.stanza tests/bindings.stanza compiler/stz-vm-driver.stanza -o vm -ccfiles tests/mycfunctions.c
 elif [ $1 == "gen-bindings" ]
 then
-  stanza compiler/stz-vm-ir.stanza compiler/stz-vm.stanza compiler/stz-vm-bindings.stanza -o vm-bindings
+  stanza compiler/stz-vm-ir.stanza compiler/stz-vm.stanza compiler/stz-built-ins.stanza compiler/stz-vm-bindings.stanza -o vm-bindings
 elif [ $1 == "bindings" ]
 then
   ./vm-bindings $2 tests/bindings.stanza
