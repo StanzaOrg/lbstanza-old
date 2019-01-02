e028b6af48b98b22d06ec21f77b2f0f056ee148d
Patrick S. Li <patrickli.2001@gmail.com>
2019-01-02 00:30:27 -0800

    Changes to notes

--- scripts/make-vm.sh
+++ scripts/make-vm.sh
@@ -3,7 +3,7 @@ then
   stanza compiler/stz-vm-ir.stanza compiler/stz-vm-analyze.stanza compiler/stz-vm.stanza compiler/stz-vm-table.stanza compiler/stz-built-ins.stanza tests/bindings.stanza compiler/stz-vm-driver.stanza -o vm -ccfiles tests/mycfunctions.c
 elif [ $1 == "gen-bindings" ]
 then
-  stanza compiler/stz-vm-ir.stanza compiler/stz-dl-ir.stanza compiler/stz-loaded-ids.stanza compiler/stz-vm.stanza compiler/stz-vm-table.stanza compiler/stz-vm-bindings.stanza -o vm-bindings
+  stanza compiler/stz-utils.stanza compiler/stz-vm-ir.stanza compiler/stz-dl-ir.stanza compiler/stz-loaded-ids.stanza compiler/stz-vm.stanza compiler/stz-vm-table.stanza compiler/stz-vm-bindings.stanza -o vm-bindings
 elif [ $1 == "bindings" ]
 then
   ./vm-bindings $2 tests/bindings.stanza
