46b596488ddbf172ad45deb4755b73f7e9bfe49c
Patrick S. Li <patrickli.2001@gmail.com>
2019-02-07 16:16:41 -0800

    Minor compilation errors.

--- compiler/cvm.c
+++ compiler/cvm.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/types.h>
+#include<stdint.h>
 
 //============================================================
 //=================== OPCODES ================================
