1b31538752eab72fcca2914229c13f96919107c7
Patrick Li <patrickli.2001@gmail.com>
2019-04-30 12:25:55 -0700

    Updated build script.

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,6 +1,9 @@
 # USAGES:
 # ./scripts/make.sh ./stanza
 
+set -e
+set -o pipefail
+
 if [ $# -lt 1 ]; then
     echo "Not enough arguments"
     exit 2
@@ -14,9 +17,10 @@ FILES="core/core.stanza \
        core/reader.stanza \
        core/parser.stanza \
        core/macro-utils.stanza \
+       compiler/line-noise-prompter.stanza \
        compiler/stz-algorithms.stanza \
-       compiler/stz-padder.stanza \
        compiler/stz-utils.stanza \
+       compiler/stz-stable-arrays.stanza \
        compiler/stz-serializer.stanza \
        compiler/stz-params.stanza \
        compiler/stz-core-macros.stanza \
@@ -26,79 +30,95 @@ FILES="core/core.stanza \
        compiler/stz-primitives.stanza \
        compiler/stz-il-ir.stanza \
        compiler/stz-tl-ir.stanza \
-       compiler/stz-kl-ir.stanza \
-       compiler/stz-pl-ir.stanza \
-       compiler/stz-tgt-ir.stanza \
-       compiler/stz-tgt-utils.stanza \
-       compiler/stz-asm-ir.stanza \
        compiler/stz-input.stanza \
+       compiler/stz-visibility.stanza \
+       compiler/stz-basic-ops.stanza \
        compiler/stz-namemap.stanza \
        compiler/stz-renamer.stanza \
        compiler/stz-resolver.stanza \
        compiler/stz-infer.stanza \
        compiler/stz-type-calculus.stanza \
        compiler/stz-type.stanza \
-       compiler/stz-type-to-kform.stanza \
-       compiler/stz-kform.stanza \
-       compiler/stz-dec-table.stanza \
-       compiler/stz-kform-to-tgt.stanza \
-       compiler/stz-khier.stanza \
+       compiler/stz-tl-to-el.stanza \
+       compiler/stz-tl-to-dl.stanza \
+       compiler/stz-el-ir.stanza \
+       compiler/stz-dl-ir.stanza \
+       compiler/stz-vm-ir.stanza \
+       compiler/stz-typeset.stanza \
+       compiler/stz-dl.stanza \
+       compiler/stz-el.stanza \
+       compiler/stz-el-infer.stanza \
+       compiler/stz-basic-blocks.stanza \
+       compiler/stz-ehier.stanza \
+       compiler/stz-vm.stanza \
+       compiler/stz-bindings.stanza \
+       compiler/stz-bindings-to-vm.stanza \
+       compiler/stz-vm-encoder.stanza \
        compiler/stz-trie.stanza \
-       compiler/stz-backend.stanza \
-       compiler/stz-reg-alloc.stanza \
-       compiler/stz-codegen.stanza \
-       compiler/stz-stitcher.stanza \
-       compiler/stz-asm-emitter.stanza \
-       compiler/stz-fuse.stanza \
-       compiler/stz-pkg.stanza \
+       compiler/stz-dyn-tree.stanza \
+       compiler/stz-dyn-bi-table.stanza \
+       compiler/stz-vm-table.stanza \
+       compiler/stz-branch-table.stanza \
+       compiler/stz-vm-analyze.stanza \
+       compiler/stz-vm-bindings.stanza \
+       compiler/stz-loaded-ids.stanza \
+       compiler/stz-el-to-vm.stanza \
+       compiler/stz-vm-normalize.stanza \
        compiler/stz-compiler.stanza \
        compiler/stz-arg-parser.stanza \
        compiler/stz-langs.stanza \
        compiler/lang-renamer.stanza \
        compiler/lang-resolver.stanza \
        compiler/lang-serializer.stanza \
+       compiler/stz-codegen2.stanza \
+       compiler/stz-reg-alloc2.stanza \
+       compiler/stz-stitcher2.stanza \
+       compiler/stz-const-pool.stanza \
+       compiler/stz-padder.stanza \
        compiler/stz-build.stanza \
+       compiler/stz-repl.stanza \
        compiler/stz-config.stanza \
+       compiler/stz-asm-ir2.stanza \
+       compiler/stz-asm-emitter.stanza \
+       compiler/stz-pkg.stanza \
+       compiler/stz-backend.stanza \
+       compiler/call-records.stanza \
        compiler/stz-main.stanza \
        compiler/stz-driver.stanza"
 
 #Delete pkg files
 rm -rf pkgs
-rm -rf fast-pkgs
 rm -rf lpkgs
-rm -rf lfast-pkgs
-rm -rf wpkgs
-rm -rf wfast-pkgs
+#rm -rf wpkgs
 
 #Create folders
 mkdir -p pkgs
-mkdir -p fast-pkgs
 mkdir -p lpkgs
-mkdir -p lfast-pkgs
-mkdir -p wpkgs
-mkdir -p wfast-pkgs
+#mkdir -p wpkgs
 
 #Compile OSX Pkgs and Executable
 echo "Compiling OSX Pkgs"
 $STANZA $FILES -pkg pkgs
 echo "Compiling OSX Executable"
-$STANZA $FILES -pkg fast-pkgs -optimize -s stanza.s
+$STANZA $FILES -pkg pkgs -optimize -s stanza.s
 
 #Compile Linux Pkgs and Executable
 echo "Compiling Linux Pkgs"
 $STANZA $FILES -pkg lpkgs -platform linux
 echo "Compiling Linux Executable"
-$STANZA $FILES -pkg lfast-pkgs -optimize -s lstanza.s -platform linux
+$STANZA $FILES -pkg lpkgs -optimize -s lstanza.s -platform linux
 
 #Compile Windows Pkgs and Executable
-echo "Compiling Windows Pkgs"
-$STANZA $FILES -pkg wpkgs -platform windows
-echo "Compiling Windows Executable"
-$STANZA $FILES -pkg wfast-pkgs -optimize -s wstanza.s -platform windows
+#echo "Compiling Windows Pkgs"
+#$STANZA $FILES -pkg wpkgs -platform windows
+#echo "Compiling Windows Executable"
+#$STANZA $FILES -pkg wfast-pkgs -optimize -s wstanza.s -platform windows
 
 #Finish on osx
-#gcc -std=gnu99 runtime/driver.c stanza.s -o stanza -DPLATFORM_OS_X -lm
+#gcc -std=gnu99 -c compiler/cvm.c -O3 -o cvm.o
+#gcc -std=gnu99 runtime/driver.c runtime/linenoise.c cvm.o stanza.s -o stanza -DPLATFORM_OS_X -lm
 #Finish on linux
-#gcc -std=gnu99 runtime/driver.c lstanza.s -o lstanza -DPLATFORM_LINUX -lm -ldl -fPIC
+#gcc -std=gnu99 -c compiler/cvm.c -O3 -o cvm.o
+#gcc -std=gnu99 runtime/driver.c runtime/linenoise.c cvm.o lstanza.s -o lstanza -DPLATFORM_LINUX -lm -ldl -fPIC
 #Finish on windows
 #gcc -std=gnu99 runtime/driver.c wstanza.s -o wstanza -DPLATFORM_WINDOWS -lm
