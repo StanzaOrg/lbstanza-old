9e3469db20226804bc8650cf47fab42cb30d0566
Patrick Li <patrickli.2001@gmail.com>
2019-07-01 11:25:09 -0700

    Add dynamic graph implementation to compiler sources.

--- scripts/make.sh
+++ scripts/make.sh
@@ -57,6 +57,8 @@ FILES="core/core.stanza \
        compiler/stz-trie.stanza \
        compiler/stz-trie-table.stanza \
        compiler/stz-hash.stanza \
+       compiler/stz-keyed-set.stanza \
+       compiler/stz-dyn-graph.stanza \
        compiler/stz-dyn-tree.stanza \
        compiler/stz-dyn-bi-table.stanza \
        compiler/stz-vm-table.stanza \
