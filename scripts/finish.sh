7ab6b2a461302673dad2f6eb765bcd46f102f4ab
Patrick Li <patrickli.2001@gmail.com>
2020-06-09 13:18:40 -0700

    Added backward compatibility flag for osx.

--- scripts/finish.sh
+++ scripts/finish.sh
@@ -1,3 +1,3 @@
 gcc -std=gnu99 -c core/sha256.c -O3 -o sha256.o
 gcc -std=gnu99 -c compiler/cvm.c -O3 -o cvm.o
-gcc -std=gnu99 runtime/driver.c runtime/linenoise.c cvm.o sha256.o stanza.s -o stanza -DPLATFORM_OS_X -lm
+gcc -std=gnu99 runtime/driver.c runtime/linenoise.c cvm.o sha256.o stanza.s -o stanza -DPLATFORM_OS_X -lm -mmacosx-version-min=10.13
