72d38fc1293c0fec948c1d8d66d4f1d90015cfe3
Tyler Lanphear <tylanphear@gmail.com>
2021-08-30 17:17:34 -0700

    build: add `wfinish.sh`
    Finish script for Windows (like `finish.sh` and `lfinish.sh`)

---
+++ scripts/wfinish.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ -z "$CC" ]]; then
+    if [[ "$(uname -s)" == MINGW* ]]; then
+        # Not cross-compiling, so just use the stock GCC
+        CC=gcc
+    else
+        echo 'Need to set $CC to a valid compiler or cross-compiler' >/dev/stderr
+        exit 2
+    fi
+fi
+
+CCFLAGS="-DPLATFORM_WINDOWS -std=gnu99 -O3 -fPIC -Wall $CCFLAGS"
+
+[[ ! -d "build" ]] && mkdir "build"
+
+"$CC" $CCFLAGS -c core/sha256.c       -o build/sha256.o
+"$CC" $CCFLAGS -c compiler/cvm.c      -o build/cvm.o
+"$CC" $CCFLAGS -c runtime/linenoise.c -o build/linenoise.o
+"$CC" $CCFLAGS -c runtime/driver.c    -o build/driver.o
+
+"$CC" \
+    build/sha256.o    \
+    build/cvm.o       \
+    build/linenoise.o \
+    build/driver.o    \
+    wstanza.s         \
+    -o wstanza -lm -lpthread -fPIC
