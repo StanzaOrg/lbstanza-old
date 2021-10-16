00456265c295a5a72044c0acc2d2519f830e011a
Patrick Li <patrickli.2001@gmail.com>
2021-10-15 18:57:15 -0700

    Add additional flags to wfinish.bat script.

--- scripts/wfinish.bat
+++ scripts/wfinish.bat
@@ -1,3 +1,3 @@
 gcc -std=gnu99 -c core/sha256.c -O3 -o build/sha256.o -I include
 gcc -std=gnu99 -c compiler/cvm.c -O3 -o build/cvm.o -I include
-gcc -std=gnu99 runtime/driver.c build/cvm.o build/sha256.o wstanza.s -o wstanza -DPLATFORM_WINDOWS -lm -I include
+gcc -std=gnu99 runtime/driver.c build/cvm.o build/sha256.o wstanza.s -o wstanza -DPLATFORM_WINDOWS -Wl,-Bstatic -lm -lpthread -I include
