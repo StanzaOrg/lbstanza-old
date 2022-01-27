3b0a6024045d8446c94ae8368ecc5cb2a069bed7
Patrick Li <patrickli.2001@gmail.com>
2022-01-26 22:36:57 -0800

    Fix issues.

--- scripts/make-bootstrap.sh
+++ scripts/make-bootstrap.sh
@@ -1,8 +1,22 @@
+# clean up everything
 rm -r pkgs/*
-git checkout merge-new-bootstrap
+unset STANZA_CONFIG
+
+# checkout the bootstrap branch and compile stanzaboot
+git checkout merge-new-gc-bootstrap
 stanza compiler/stanza.proj stz/driver -o stanzaboot -optimize -verbose
+
+# checkout the merge-new-gc branch
 git checkout merge-new-gc
+
+# set this folder as the current installation.
 mkdir tempinstall
 ./stanzaboot install -platform os-x -path tempinstall
+export STANZA_CONFIG=$PWD/tempinstall
+
+# Use the bootstrap compiler to compile the new compiler and new pkgs
 ./stanzaboot core/stanza.proj compiler/stanza.proj -o newstanza -optimize -verbose
 ./newstanza core/stanza.proj core collections -pkg pkgs
+
+# Display finish
+./newstanza version
