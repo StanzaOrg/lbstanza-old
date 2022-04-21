91da657510bc8724761edede986f9163d84cda11
Patrick Li <patrickli.2001@gmail.com>
2022-04-21 16:14:55 -0700

    Prevent long running tests from running in interpreter.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,3 +1,3 @@
-stanza run-test build-stanza.proj tests/stanza.proj stz/stanza-tests
+stanza run-test build-stanza.proj tests/stanza.proj stz/stanza-tests -not-tagged long
 stanza compile-test build-stanza.proj tests/stanza.proj stz/stanza-tests -o build/stanza-tests
 ./build/stanza-tests
