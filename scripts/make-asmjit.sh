dafad801484b96d3234d4f0f00ae19c3e7fe251e
Patrick Li <patrickli.2001@gmail.com>
2022-05-09 18:16:32 -0700

    Add comments to make-asmjit.sh.

--- scripts/make-asmjit.sh
+++ scripts/make-asmjit.sh
@@ -1,15 +1,18 @@
-HERE="$(dirname "${BASH_SOURCE[0]}")"
-
+# Set early failure flags
 set -e
 set -o pipefail
 
+# Sanity check: Ensure we are calling
+# this script with the right number of arguments.
 if [ $# -lt 1 ]; then
     echo "Not enough arguments"
     exit 2
 fi
 
+# Set OPTION as the first argument.
 OPTION="$1"
 
+# Compute FILENAME based upon setting of OPTION.
 case "$OPTION" in
     os-x)
         FILENAME="libasmjit-os-x.a" ;;
@@ -24,7 +27,13 @@ Error: unsupported option.
 EOF
 esac
 
+# Call CMake to build AsmJIT
 cmake -S libs/asmjit -B build/asmjit
 cmake --build build/asmjit
+
+# Move the built artifact to the right place with
+# the right filename.
 mv build/asmjit/libasmjit.a bin/$FILENAME
+
+# Remove the old build folder
 rm -r build/asmjit
