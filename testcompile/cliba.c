df6a1fb351848d933757ec7671ed7a2aa9f83afc
Patrick Li <patrickli.2001@gmail.com>
2022-11-11 08:45:59 -0800

    Created test project with compilation log.

---
+++ testcompile/cliba.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void liba_helper () {
+  printf("Helper function in LibA\n");
+  printf("Big important things are happening.\n");
+}
---
+++ testcompile/clibb.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void libb_func () {
+  printf("Helper function in LibB\n");
+  printf("Small useful things are happening.\n");
+}
