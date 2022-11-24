51026607d88af55bfa746ce7550b90b61cb020c1
Patrick Li <patrickli.2001@gmail.com>
2022-11-23 23:32:03 -0800

    Add GNU extensions flag for linux.

--- scripts/lfinish.sh
+++ scripts/lfinish.sh
@@ -1,4 +1,4 @@
 scripts/make-asmjit.sh linux
 gcc -std=gnu99 -c core/sha256.c -O3 -o build/sha256.o -fPIC -I include
 gcc -std=gnu99 -c compiler/cvm.c -O3 -o build/cvm.o -fPIC -I include
-gcc -std=gnu99 core/threadedreader.c runtime/driver.c runtime/linenoise.c build/cvm.o build/sha256.o lstanza.s -o lstanza -DPLATFORM_LINUX -lm -ldl -fPIC -I include -Lbin -lasmjit-linux -lstdc++ -lrt -lpthread
+gcc -std=gnu99 core/threadedreader.c runtime/driver.c runtime/linenoise.c build/cvm.o build/sha256.o lstanza.s -o lstanza -DPLATFORM_LINUX -D_GNU_SOURCE -lm -ldl -fPIC -I include -Lbin -lasmjit-linux -lstdc++ -lrt -lpthread
