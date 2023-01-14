30477cf771f775a666a63fd92b1269cbfef1bd41
Patrick Li <patrickli.2001@gmail.com>
2023-01-13 17:56:40 -0800

    Fix bug in call records computation.

--- scripts/finish.sh
+++ scripts/finish.sh
@@ -1,4 +1,4 @@
 scripts/make-asmjit.sh os-x
 gcc -std=gnu99 -c core/sha256.c -O3 -o build/sha256.o -I include
 gcc -std=gnu99 -c compiler/cvm.c -O3 -o build/cvm.o -I include
-gcc -std=gnu99 core/threadedreader.c runtime/driver.c runtime/linenoise.c build/cvm.o build/sha256.o stanza.s -o stanza -DPLATFORM_OS_X -lm -mmacosx-version-min=10.13 -I include -Lbin -lasmjit-os-x -lc++
+gcc -std=gnu99 core/threadedreader.c runtime/driver.c compiler/exec-alloc.c runtime/linenoise.c build/cvm.o build/sha256.o stanza.s -o stanza -DPLATFORM_OS_X -lm -I include -Lbin -lasmjit-os-x -lc++
--- scripts/wfinish.bat
+++ scripts/wfinish.bat
@@ -1,4 +1,4 @@
 call scripts\make-asmjit.bat
 gcc -std=gnu99 -c core/sha256.c -O3 -o build/sha256.o -I include
 gcc -std=gnu99 -c compiler/cvm.c -O3 -o build/cvm.o -I include
-gcc -std=gnu99 core/threadedreader.c core/dynamic-library.c runtime/driver.c build/cvm.o build/sha256.o wstanza.s -o wstanza -DPLATFORM_WINDOWS -Wl,-Bstatic -lm -lpthread -I include -Lbin -lasmjit-windows -lstdc++
+gcc -std=gnu99 core/threadedreader.c core/dynamic-library.c compiler/exec-alloc.c runtime/driver.c build/cvm.o build/sha256.o wstanza.s -o wstanza -DPLATFORM_WINDOWS -Wl,-Bstatic -lm -lpthread -I include -Lbin -lasmjit-windows -lstdc++
