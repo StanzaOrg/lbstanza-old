d3c74f3257249c88921af9657d0e2adaae574fc4
Patrick Li <patrickli.2001@gmail.com>
2023-03-14 11:46:36 -0700

    Integrate changes into `bootstrap-stanza.sh`.

--- ci/bootstrap-stanza.sh
+++ ci/bootstrap-stanza.sh
@@ -43,15 +43,16 @@ fi
 # assume the current directory is the root of a stanza repository
 
 ${STANZA} compile-macros \
-       compiler/ast-printer-lang.stanza \
-       compiler/renamer-lang.stanza \
-       compiler/reader-lang.stanza \
-       compiler/resolver-lang.stanza \
-       compiler/ast-lang2.stanza \
-       compiler/message-lang.stanza \
-       -o bootstrapmacros
+  compiler/fastio-serializer-macros.stanza \
+  compiler/renamer-lang.stanza \
+  compiler/check-lang.stanza \
+  compiler/ast-lang2.stanza \
+  compiler/ast-lang.stanza \
+  compiler/resolver-lang.stanza \
+  compiler/message-lang.stanza \
+  compiler/ast-printer-lang.stanza \
+  compiler/reader-lang.stanza \
+  -o bootstrap-macros 
 
-${STANZA} compier/stanza.proj stz-driver -o stanzatemp -flags BOOTSTRAP
-
-${STANZA} build-stanza.proj stz/driver -o stanzatemp -flags BOOTSTRAP -optimize -verbose
+${STANZA} build-stanza.proj stz/driver -o stanzatemp -macros bootstrap-macros -flags BOOTSTRAP -optimize
 
